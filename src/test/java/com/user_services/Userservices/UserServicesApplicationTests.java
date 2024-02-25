d608faae5b0de3a5bf6cb97cddc1d4146298709c
ASHISHVERMA114142 <ashishverma114142@gmail.com>
2024-02-26 00:27:47 +0530

    initial commit

---
+++ src/test/java/com/user_services/Userservices/UserServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_services.Userservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
